c33546398e1eea75c8a628afeea081705a3cbf14
PiotrDuma <piotrduma2014@gmail.com>
2023-11-07 19:37:56 +0100

    init project

---
+++ src/main/java/com/github/PiotrDuma/foodmart/FoodmartRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.PiotrDuma.foodmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodmartRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodmartRestfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/PiotrDuma/foodmart/FoodmartRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.PiotrDuma.foodmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodmartRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
