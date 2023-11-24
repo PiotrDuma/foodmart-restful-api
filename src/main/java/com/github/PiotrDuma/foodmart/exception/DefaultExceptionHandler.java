1997d67f868523844e81b306b0f2cf908260409f
PiotrDuma <piotrduma2014@gmail.com>
2023-11-24 07:38:54 +0100

    add exception handling

---
+++ src/main/java/com/github/PiotrDuma/foodmart/exception/DefaultExceptionHandler.java
@@ -0,0 +1,57 @@
+package com.github.PiotrDuma.foodmart.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@RestControllerAdvice
+class DefaultExceptionHandler {
+
+  @ExceptionHandler({NoHandlerFoundException.class})
+  private ResponseEntity<ExceptionDto> handleNoHandlerFoundException(Exception ex,
+      HttpServletRequest request) {
+    ExceptionDto exceptionDto = new ExceptionDto(
+        request.getRequestURI(),
+        "The requested URL does not exist",
+        HttpStatus.NOT_FOUND.value(),
+        LocalDateTime.now()
+    );
+    return new ResponseEntity<>(exceptionDto, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler({ResourceNotFoundException.class})
+  private ResponseEntity<ExceptionDto> handleResourceNotFoundException(Exception ex,
+      HttpServletRequest request) {
+    ExceptionDto exceptionDto = new ExceptionDto(
+        request.getRequestURI(),
+        ex.getMessage(),
+        HttpStatus.NOT_FOUND.value(),
+        LocalDateTime.now()
+    );
+    return new ResponseEntity<>(exceptionDto, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler({Exception.class})
+  private ResponseEntity<ExceptionDto> handleDefaultException(Exception ex,
+      HttpServletRequest request) {
+    ExceptionDto exceptionDto = new ExceptionDto(
+        request.getRequestURI(),
+        ex.getMessage(),
+        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+        LocalDateTime.now()
+    );
+    return new ResponseEntity<>(exceptionDto, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  private record ExceptionDto(
+      String requestURL,
+      String errorMessage,
+      int statusCode,
+      LocalDateTime timestamp
+  ) {
+  }
+}
